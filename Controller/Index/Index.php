b79ee37bb08b1761f53b7db441992f5937eda766
Hemanth007 <hemanthkumarpatnala1@gmail.com>
2023-08-03 14:24:53 +0530

    demo helloworld commit

---
+++ Controller/Index/Index.php
@@ -0,0 +1,38 @@
+<?php
+namespace Demo\Hello\Controller\Index;
+use Magento\Framework\App\Action\Action;
+use Magento\Framework\App\Action\Context;
+use Magento\Framework\View\Result\PageFactory;
+/**
+ * Demo Hello Landing page Index Controller.
+ */
+class Index extends Action
+{
+    /**
+     * @var PageFactory
+     */
+    protected $_resultPageFactory;
+    /**
+     * @param Context     $context
+     * @param PageFactory $resultPageFactory
+     */
+    public function __construct(
+        Context $context,
+        PageFactory $resultPageFactory
+    ) {
+        $this->_resultPageFactory = $resultPageFactory;
+        parent::__construct($context);
+    }
+    /**
+     * Hello Landing page.
+     *
+     * @return \Magento\Framework\View\Result\Page
+     */
+    public function execute()
+    {
+        $resultPage = $this->_resultPageFactory->create();
+        // Set title of page
+        $resultPage->getConfig()->getTitle()->set(__('Sample module in Magento 2 by Webkul'));
+        return $resultPage;
+    }
+}
